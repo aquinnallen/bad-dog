3b1a89d1c3af856e445952d80899e28aa3650ef8
Dom or Quinn <DomorQuinn@someemail.com>
2024-02-15 02:07:25 +0000

    Slowly trying to add documentation to the readme + fixing recognition

--- frontend/recognition.py
+++ frontend/recognition.py
@@ -1,20 +1,31 @@
-from flask import flask, request, render_template, jsonify
+import os
+from flask import Flask, request, render_template, jsonify
 from flask_cors import CORS 
 from speechbrain.pretrained.interfaces import foreign_class
-classifier = foreign_class(source="speechbrain/emotion-recognition-wav2vec2-IEMOCAP",pymodule_file="custom_interface.py",className = "CustomEncoderWav2vec2Classifier")
 
-app = flask(__name__)
+
+counter = 0
+
+classifier = foreign_class(source="speechbrain/emotion-recognition-wav2vec2-IEMOCAP", pymodule_file="custom_interface.py", classname="CustomEncoderWav2vec2Classifier")
+
+app = Flask(__name__)
+CORS(app)
+
 @app.route("/",methods= ["GET", "POST"])
-def.hello():
+def hello():
     return render_template("index.html")
 
 @app.route("/upload_audio_file", methods=["POST"])
 def upload_audio_file():
-    audioFile = request.files["audio_file"]
-    out_prob, score, index, text_lab = classifier.classify_file(audioFile)
+    audioFile = request.files["audioFile"]
+    path = "tmp"+str(counter)+".wav"
+    audioFile.save(path)
+    out_prob, score, index, text_lab = classifier.classify_file(path)    
+    resp = {"success": True, "response": text_lab[0]}
+    return resp
 
-    resp = {"success": True, "response": "file saved!"}
-    return text_lab
 
 
 
+if __name__=='__main__':
+  app.run(host='0.0.0.0', port=5000)
